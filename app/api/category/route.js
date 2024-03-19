62ae91492644f3e0786210f2a95d7bd6fb01ad4a
jeevaphysicist <jeevaphysicist@gmail.com>
2024-03-19 15:26:49 +0530

    code submission

--- app/api/category/route.js
+++ app/api/category/route.js
@@ -1,12 +1,12 @@
 import { ConnectToDB } from "@utils/database";
-import Category from "@models/category";
+import CategoryModel from "@models/category";
 
 // Update Category
 export const GET = async (req) => { 
   try {
     await ConnectToDB();
     // Find Category is Available or not in database ( Category Collection )
-    let category = await Category.find();
+    let category = await CategoryModel.find();
     return new Response(JSON.stringify(category), { status: 200 });
   } catch (error) {
      return new Response(
